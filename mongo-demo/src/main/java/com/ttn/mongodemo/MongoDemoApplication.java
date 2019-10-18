e0d83c6ac3a8740e8c673aba114b5b859406f35a
Manisha Goyal <manisha.goyal@tothenew.com>
2019-10-18 15:00:57 +0530

    WIP:Swagger

--- mongo-demo/src/main/java/com/ttn/mongodemo/MongoDemoApplication.java
+++ mongo-demo/src/main/java/com/ttn/mongodemo/MongoDemoApplication.java
@@ -2,15 +2,24 @@ package com.ttn.mongodemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableSwagger2
 public class MongoDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoDemoApplication.class, args);
 	}
 
+//	@Bean
+//	public Docket productApi() {
+//		return new Docket(DocumentationType.SWAGGER_2).select()
+//				.apis(RequestHandlerSelectors.basePackage("com.tutorialspoint.swaggerdemo")).build();
+//	}
 }
